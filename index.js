7de60a7988a4445025e39912c6f98fb45ea1c6fe
Prathamesh Madniwale <61145586+prathamesh2525@users.noreply.github.com>
2023-07-21 18:22:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
-var bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
 
 const urlRouter = require("./routes/url")
 const redirectURLRouter = require("./routes/redirect")
@@ -10,6 +12,7 @@ connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
   console.log("Connected to MongoDB")
 })
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use("/url", urlRouter)
 
